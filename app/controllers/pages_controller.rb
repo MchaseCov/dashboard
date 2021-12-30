d92d07c16d42fe314f193c52a27034d9a00db005
Chase Covington <mchasecov@gmail.com>
2021-12-30 17:03:51 -0500

    feat: live page has dummy content

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,5 @@
 class PagesController < ApplicationController
   def main
-    client = Octokit::Client.new(access_token: ENV['github_token'])
-    @repos = client.repos({}, query: { type: 'owner', sort: 'pushed', per_page: 6 })
     @recent_notepad = Notepad.order('updated_at').last
   end
 end
